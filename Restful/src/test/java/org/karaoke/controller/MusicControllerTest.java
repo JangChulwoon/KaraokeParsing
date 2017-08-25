b3ac302b79ba40878aaf3a97b7ae908d75a1ff8e
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-08-25 21:16:38 +0900

    Controller test case 작성

---
+++ Restful/src/test/java/org/karaoke/controller/MusicControllerTest.java
@@ -0,0 +1,66 @@
+package org.karaoke.controller;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.hasSize;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.karaoke.domain.Karaoke;
+import org.karaoke.service.KaraokeService;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring/root-context.xml",
+		"classpath:spring/appServlet/servlet-context.xml" })
+public class MusicControllerTest {
+
+	private MockMvc mvc;
+
+	MusicController controller;
+
+	@Autowired
+	WebApplicationContext context;
+
+	@Mock
+	KaraokeService service;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		controller = new MusicController();
+		controller.setKaraokeService(service);
+		mvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+
+	@Test
+	public void shouldServiceInjection() throws Exception {
+		String company = "TJ", type = "song", title = "title";
+		String url = "/" + company + "/" + type + "/" + title;
+		List<Karaoke> list = new ArrayList<Karaoke>();
+		Karaoke karaoke = new Karaoke();
+		karaoke.setNumber("number");
+		list.add(karaoke);
+		when(service.makeKaraokeNumber(company, type, title)).thenReturn(list);
+
+		mvc.perform(get(url)).andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$",hasSize(1)))
+				.andExpect(jsonPath("$[0].number").value("number"));
+		verify(service,times(1)).makeKaraokeNumber(company, type, title);
+	}
+}
--- Restful/src/test/java/org/karaoke/service/KaraokeServiceTest.java
+++ Restful/src/test/java/org/karaoke/service/KaraokeServiceTest.java
@@ -5,13 +5,21 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.apache.log4j.Logger;
+import org.aspectj.lang.annotation.Before;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.karaoke.controller.MusicController;
 import org.karaoke.domain.Karaoke;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -21,9 +29,9 @@ public class KaraokeServiceTest {
 	
 	@Inject
 	KaraokeService service;
-		
+	
 	@Test
-	public void shouldServiceInjection() {
+	public void shouldParseKaraoke() {
 		// when
 		List<Karaoke> karaoke = service.makeKaraokeNumber("KY", "song", "첫눈");
 		

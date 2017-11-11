6a01be12e11dd588c1ceb9571b847663e6b54667
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-11-11 17:11:55 +0900

    Simply make restController

--- karaoke/src/main/java/org/karaoke/restcontroller/KaraokeController.java
+++ karaoke/src/main/java/org/karaoke/restcontroller/KaraokeController.java
@@ -4,6 +4,7 @@ package org.karaoke.restcontroller;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class KaraokeController {
 
     @GetMapping("/{company}/{category}/{word}")
-    public String selectKaraoke(){
-        log.info("Hello?? ");
+    public String selectKaraoke(@PathVariable String company, @PathVariable String category, @PathVariable String word){
+        log.info("company : {} , category : {} , word : {}",company,category,word);
         return "{ 'hello' : 'world'}";
     }
 

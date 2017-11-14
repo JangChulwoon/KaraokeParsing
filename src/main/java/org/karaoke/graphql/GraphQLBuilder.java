c10b054467c0666ef14a98078213eac0aa238bfd
jangchulwoon <luisue@gmail.com>
2017-11-14 22:32:31 +0900

    Add GraphQL Union & Interface Code - Demo
    데모 버전의 Union - Interface 코드를 추가함
    구조에 대해 조금더 생각해 봐야할것 같음

--- src/main/java/org/karaoke/graphql/GraphQLBuilder.java
+++ src/main/java/org/karaoke/graphql/GraphQLBuilder.java
@@ -1,6 +1,7 @@
 package org.karaoke.graphql;
 
 import graphql.GraphQL;
+import graphql.TypeResolutionEnvironment;
 import graphql.schema.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static graphql.Scalars.GraphQLInt;
 import static graphql.Scalars.GraphQLString;
@@ -22,54 +28,102 @@ public class GraphQLBuilder {
     @Autowired
     DataFetcher dataFetcher;
 
-    private GraphQLObjectType Karaoke = newObject()
-            .name("Karaoke")
-            .description("노래방 번호,제목,가수명을 갖고있는 Type 입니다. ")
-            .field(newFieldDefinition()
-                    .name("number")
-                    .type(GraphQLString))
-            .field(newFieldDefinition()
-                    .name("title")
-                    .type(GraphQLString))
-            .field(newFieldDefinition()
-                    .name("singer")
-                    .type(GraphQLString))
-            .build();
-
-    private GraphQLEnumType company = newEnum()
-            .name("COMPANY")
-            .value("KY")
-            .value("TJ")
-            .build();
-
-    private GraphQLEnumType category = newEnum()
-            .name("CATEGORY")
-            .value("SINGER")
-            .value("SONG")
-            .build();
-
-    private GraphQLInputObjectType karaoke = GraphQLInputObjectType.newInputObject()
-            .name("karaoke")
-            .field(GraphQLInputObjectField.newInputObjectField()
-                    .name("company")
-                    .type(company)
-                    .build())
-            .field(GraphQLInputObjectField.newInputObjectField()
-                    .name("category")
-                    .type(category)
-                    .build())
-            .field(GraphQLInputObjectField.newInputObjectField()
-                    .name("keyword")
-                    .type(GraphQLString)
-                    .build())
-            .build();
-
-    GraphQLObjectType objectType;
     private GraphQL graphQL;
+
+    // 이 코드는 조금 수정해야겟다.
     @PostConstruct
     public void setUp(){
-        objectType = newObject()
-                .name("selectKaraoke")
+
+        GraphQLObjectType Karaoke = newObject()
+                .name("Karaoke")
+                .description("노래방 번호,제목,가수명을 갖고있는 Type 입니다. ")
+                .field(newFieldDefinition()
+                        .name("number")
+                        .type(GraphQLString))
+                .field(newFieldDefinition()
+                        .name("title")
+                        .type(GraphQLString))
+                .field(newFieldDefinition()
+                        .name("singer")
+                        .type(GraphQLString))
+                .build();
+
+         GraphQLEnumType company = newEnum()
+                .name("COMPANY")
+                .value("KY")
+                .value("TJ")
+                .build();
+
+         GraphQLEnumType category = newEnum()
+                .name("CATEGORY")
+                .value("SINGER")
+                .value("SONG")
+                .build();
+
+        GraphQLInputObjectType karaoke = GraphQLInputObjectType.newInputObject()
+                .name("karaoke")
+                .field(GraphQLInputObjectField.newInputObjectField()
+                        .name("company")
+                        .type(company)
+                        .build())
+                .field(GraphQLInputObjectField.newInputObjectField()
+                        .name("category")
+                        .type(category)
+                        .build())
+                .field(GraphQLInputObjectField.newInputObjectField()
+                        .name("keyword")
+                        .type(GraphQLString)
+                        .build())
+                .build();
+
+
+
+
+
+        // 여기부턴 학습용
+
+        GraphQLObjectType inter1 = newObject()
+                .name("Karaoke1")
+                .description("노래방 번호,제목,가수명을 갖고있는 Type 입니다. ")
+                .field(newFieldDefinition()
+                        .name("name")
+                        .type(GraphQLString))
+                .field(newFieldDefinition()
+                        .name("title")
+                        .type(GraphQLString))
+                .build();
+        GraphQLObjectType inter2 = newObject()
+                .name("Karaoke2")
+                .description("노래방 번호,제목,가수명을 갖고있는 Type 입니다. ")
+                .field(newFieldDefinition()
+                        .name("name")
+                        .type(GraphQLString))
+                .field(newFieldDefinition()
+                        .name("tt")
+                        .type(GraphQLString))
+                .build();
+
+        GraphQLUnionType PetType = GraphQLUnionType.newUnionType()
+                .name("Hello")
+                .possibleType(inter1)
+                .possibleType(inter2)
+                .typeResolver(
+                        new TypeResolver() {
+                            @Override
+                            public GraphQLObjectType getType(TypeResolutionEnvironment env) {
+                                log.info("{}", env.getObject().toString());
+                                if(env.getObject() instanceof  Map){
+                                    return inter2;
+                                }else{
+                                    return inter1;
+                                }
+
+                            }
+                        }
+                )
+                .build();
+
+        GraphQLObjectType objectType = newObject().name("selectKaraoke")
                 .field(newFieldDefinition()
                         .name("Karaoke")
                         .type(new GraphQLList(Karaoke))
@@ -79,17 +133,26 @@ public class GraphQLBuilder {
                                 .type(karaoke))
                         .argument(GraphQLArgument.newArgument()
                                 .name("page")
-                                .type(GraphQLInt))
-                )
+                                .type(GraphQLInt)))
+                .field(newFieldDefinition()
+                        .name("Karaoke11")
+                        .type(PetType)
+                        .dataFetcher((env)-> {
+                            Map<String ,String> map = new HashMap<>();
+                            map.put("title","제목");
+                            map.put("tt","제목1");
+                            return map;
+                        } ))
                 .build();
+
 
         GraphQLSchema schema = GraphQLSchema.newSchema()
                 .query(objectType)
                 .build();
+
         graphQL = GraphQL.newGraphQL(schema)
                 .build();
     }
-
 
     @Bean
     public GraphQL getGraphQL() {

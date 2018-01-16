705f7d35676bfa47691662289fe39b46f2b9cb1d
Jangchulwoon <lusiue@naver.com>
2018-01-16 21:59:16 +0900

    Write controller to enroll query
    쿼리 등록 및 보여주기 위한 컨트롤러 생성

--- src/main/java/org/karaoke/restcontroller/PersistController.java
+++ src/main/java/org/karaoke/restcontroller/PersistController.java
@@ -3,18 +3,28 @@ package org.karaoke.restcontroller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.stream.Stream;
 
 @RestController
 @Slf4j
 public class PersistController {
 
 
-    @GetMapping("/admin")
-    public ResponseEntity<String> addQuery(String query , String message){
+    @Autowired
+    Map<String,String> persistentQuery;
 
-        return ResponseEntity.ok("OK");
+    @GetMapping("/queries")
+    public String selectAll(){ // key : value 인 json 으로 추후 바꿀 것
+        return persistentQuery.toString();
+    }
+
+    @PostMapping("/queries")
+    public ResponseEntity<String> selectAll(@RequestBody Map<String,String> request){
+        persistentQuery.put(request.get("key"),request.get("query"));
+        return ResponseEntity.ok("Success");
     }
 
 }
